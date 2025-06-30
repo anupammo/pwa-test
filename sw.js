177fe6141cdfe0bad50891c3a3d2a2cf93180174
Anupam Mondal <a09051985@gmail.com>
2025-07-01 04:33:55 +0530

    Enhance Caching

--- sw.js
+++ sw.js
@@ -20,7 +20,14 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request).then(fetchResponse => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request.url, fetchResponse.clone());
+            return fetchResponse;
+          });
+        });
+      })
   );
 });
 
